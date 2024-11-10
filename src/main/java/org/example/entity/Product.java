5e96da70215270233556c39ec419f9dd1102e8f0
Kirill Dmitriev <kirilldmitriev20015@gmail.com>
2024-11-11 01:52:59 +0500

    add products table

--- src/main/java/org/example/entity/Product.java
+++ src/main/java/org/example/entity/Product.java
@@ -3,26 +3,35 @@ package org.example.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "products")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty(message = "Product name is required")
     @Size(max = 255, message = "Product name should be less than 255 characters")
+    @Column(nullable = false, length = 255)
     private String name;
 
     @Size(max = 4096, message = "Description should be less than 4096 characters")
+    @Column(length = 4096)
     private String description;
 
     @Min(value = 0, message = "Price should be greater than or equal to 0")
+    @Column(nullable = false)
     private double price = 0.0;
 
+    @Column(nullable = false)
     private boolean inStock = false;
 }
 

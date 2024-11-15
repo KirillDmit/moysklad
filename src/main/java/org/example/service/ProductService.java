302eef2fc160e70a9b0e0c9b36fab4123538c9b6
Kirill Dmitriev <kirilldmitriev20015@gmail.com>
2024-11-16 00:06:49 +0500

    update product service
    add get filtered products

--- src/main/java/org/example/service/ProductService.java
+++ src/main/java/org/example/service/ProductService.java
@@ -2,7 +2,12 @@ package org.example.service;
 
 import org.example.entity.Product;
 import org.example.repository.ProductRepository;
+import org.example.repository.ProductSpecifications;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,6 +29,34 @@ public class ProductService {
 
     public Optional<Product> getProductById(Long id) {
         return productRepository.findById(id);
+    }
+
+    public List<Product> getFilteredProducts(String name, Double minPrice, Double maxPrice, Boolean inStock, String sortBy, Integer limit) {
+
+        Specification<Product> spec = Specification.where(null);
+
+        if (name != null && !name.isEmpty()) {
+            spec = spec.and(ProductSpecifications.nameContains(name));
+        }
+        if (minPrice != null) {
+            spec = spec.and(ProductSpecifications.priceGreaterThanOrEqualTo(minPrice));
+        }
+        if (maxPrice != null) {
+            spec = spec.and(ProductSpecifications.priceLessThanOrEqualTo(maxPrice));
+        }
+        if (inStock != null) {
+            spec = spec.and(ProductSpecifications.isInStock(inStock));
+        }
+
+        Sort sort = Sort.unsorted();
+        if ("name".equalsIgnoreCase(sortBy)) {
+            sort = Sort.by("name");
+        } else if ("price".equalsIgnoreCase(sortBy)) {
+            sort = Sort.by("price");
+        }
+
+        Pageable pageable = PageRequest.of(0, limit, sort);
+        return productRepository.findAll(spec, pageable).getContent();
     }
 
     public Product createProduct(Product product) {
